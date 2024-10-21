ef0846648ea1bf9384753af1ec854218b3d19532
Ahmed Aboshendy <a7meddon123@gmail.com>
2024-10-21 23:14:24 +0300

    installing @nestjs/mapped-types

--- src/user/dtos/update-user.dto.ts
+++ src/user/dtos/update-user.dto.ts
@@ -1,5 +1,6 @@
-export class UpdateUserDto {
-    username?: string;
-    email?: string;
-    age?: number;
+import { PartialType } from "@nestjs/mapped-types";
+import { CreateUserDto } from "./create-user.dto";
+
+export class UpdateUserDto extends PartialType(CreateUserDto) {// to inhiret from CreateUserDto and to make it optional i will use 
+
 }
